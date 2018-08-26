eebed8a28d8dd023b915a52836cf13f9c9ffb77f
a-tsu <lab_is_truth@yahoo.co.jp>
2018-08-26 23:57:18 +0900

    fix

---
+++ README.md
@@ -0,0 +1,3 @@
+reference:
+- https://github.com/apache/incubator-pulsar/blob/master/pulsar-client-cpp/lib/auth/athenz/ZTSClient.cc
+- https://qiita.com/alaif-net@github/items/e4432c374985dc38e038
--- curl.cpp
+++ curl.cpp
@@ -2,13 +2,40 @@
 #include <iostream>
 #include <curl/curl.h>
 
+static size_t curlWriteCallback(void *contents, size_t size, size_t nmemb, void *responseDataPtr) {
+    ((std::string *)responseDataPtr)->append((char *)contents, size * nmemb);
+        return size * nmemb;
+        }
+
 int main() 
 {
     CURL *handle;
     CURLcode res;
+    std::string responseData;
     handle = curl_easy_init();
-    curl_easy_setopt(handle, CURLOPT_URL, "yahoo.co.jp");
+
+    // url
+    curl_easy_setopt(handle, CURLOPT_URL, "https://85742f9d-3f60-40a3-b456-487e0815417b.mock.pstmn.io/trial");
+    // res func
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curlWriteCallback);
+    // get res data
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &responseData);
+    std::cout << "setting ready!\n";
     res = curl_easy_perform(handle);
+    if (res != CURLE_OK) {
+    	std::cerr << "curl error!\n";
+    	return 1;
+    } else {
+        long response_code;
+        curl_easy_getinfo(handle, CURLINFO_RESPONSE_CODE, &response_code);
+        if (response_code == 200) {
+        std::cout << responseData << "\n";
+        return 0;
+        } else {
+    	    std::cerr << "curl status error!\n";
+    	    return 1;
+        }
+    }
     std::cout << "Hello, World!\n";
     return 0;
 }
